79ff2a6b17897519be27e84f343eac5d09f6f8e9
Steve Powell <steve@rabbitmq.com>
2014-07-14 16:29:29 +0100

    Added Namer interface method. Added descriptive comments to Namer and Collector interfaces.

--- libcontainer_api/collecting/collector.go
+++ libcontainer_api/collecting/collector.go
@@ -1,18 +1,25 @@
 package collecting
 
 import (
-	"github.com/cf-guardian/prototype/libcontainer_api"
-	"github.com/cf-guardian/prototype/libcontainer_api/naming"
+	api "github.com/cf-guardian/prototype/libcontainer_api"
+	naming "github.com/cf-guardian/prototype/libcontainer_api/naming"
 )
 
+// Manage a collection of named containers.
+// Each container is represented by its internal name (CName), and associated with its (external) Name by
+// the collection.
 type Collector interface {
 
-	Add(name libcontainer_api.Name, cname naming.CName) error
+	// Add a Container (identified by cname) to this collection with the given name.
+	Add(name api.Name, cname naming.CName) error
 
-	Remove(name libcontainer_api.Name) error
+	// Remove the given name from this collection.
+	Remove(name api.Name) error
 
-	Get(name libcontainer_api.Name) (naming.CName, error)
+	// Return the internal CName associated with the given name from this collection.
+	Get(name api.Name) (naming.CName, error)
 
-	Names() []libcontainer_api.Name
+	// Return the names in this collection.
+	Names() []api.Name
 
 }
