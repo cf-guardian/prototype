04f0930385efb38aa25f4573563dd45391e6a0bf
Glyn Normington <gnormington@pivotal.io>
2014-10-02 16:12:06 +0100

    callbacks do not need to be exported

--- namespaces/mount_namespace/mount_namespace.go
+++ namespaces/mount_namespace/mount_namespace.go
@@ -12,10 +12,10 @@ const Id = syscall.CLONE_NEWNS
 const defaultMountFlags = syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 
 func init() {
-	namespaces.RegisterCallback(syscall.CLONE_NEWNS, InNamespace)
+	namespaces.RegisterCallback(syscall.CLONE_NEWNS, initialise)
 }
 
-func InNamespace() error {
+func initialise() error {
 	// Remount /proc
 	if err := syscall.Unmount("/proc", syscall.MNT_DETACH); err != nil {
 		return fmt.Errorf("syscall.Unmount error", err.Error())
